f045a8cc0c2a440560fd32525f63506dcedceb36
ShikenNuggets <carter.rennick@gmail.com>
2024-07-01 21:41:27 -0400

    Custom SharedPtr implementation

---
+++ Jammer/Memory/ReferenceCount.h
@@ -0,0 +1,40 @@
+#ifndef JAMMER_MEMORY_REFERENCE_COUNT_H
+#define JAMMER_MEMORY_REFERENCE_COUNT_H
+
+#include "Debug.h"
+
+namespace Jammer{
+	class ReferenceCount{
+	public:
+		ReferenceCount() : count(0){}
+
+		void Increment(){
+			count++;
+		}
+
+		void Decrement(){
+			J_BASIC_ASSERT(count > 0);
+			if(count == 0){
+				return;
+			}
+
+			count--;
+		}
+
+		void operator ++(){
+			Increment();
+		}
+
+		void operator --(){
+			Decrement();
+		}
+
+		size_t Count() const{ return count; }
+		bool HasReferences() const{ return count > 0; }
+
+	private:
+		size_t count;
+	};
+}
+
+#endif //!JAMMER_MEMORY_REFERENCE_COUNT_H
---
+++ Jammer/Memory/SharedPtr.h
@@ -0,0 +1,72 @@
+#ifndef JAMMER_MEMORY_SHARED_PTR_H
+#define JAMMER_MEMORY_SHARED_PTR_H
+
+#include "ReferenceCount.h"
+
+namespace Jammer{
+	template <class T>
+	class SharedPtr{
+	public:
+		SharedPtr() : object(nullptr), refCount(nullptr){}
+
+		SharedPtr(T* object_) : object(object_), refCount(new ReferenceCount()){
+			J_BASIC_ASSERT(refCount != nullptr);
+			*refCount++;
+		}
+
+		SharedPtr(const SharedPtr<T>& other_) : object(other_.object), refCount(other_.refCount){
+			J_BASIC_ASSERT(refCount != nullptr);
+			*refCount++;
+		}
+
+		virtual ~SharedPtr(){
+			J_ASSERT((refCount == nullptr && object == nullptr) || (refCount != nullptr && refCount != nullptr), "If refCount or object is null, the other MUST be null");
+			if(refCount == nullptr){
+				return;
+			}
+
+			DecrementReferenceCountAndCleanup();
+		}
+
+		SharedPtr& operator=(const SharedPtr<T>& other_){
+			if(this == &other_){
+				return;
+			}
+
+			DecrementReferenceCountAndCleanup();
+
+			object = other_.object;
+			refCount = other_.refCount;
+
+			if(refCount != nullptr){
+				*refCount++;
+			}
+
+			return *this;
+		}
+
+		T& operator*(){ return *object; }
+		T* operator->(){ return object; }
+
+	private:
+		T* object;
+		ReferenceCount* refCount;
+
+		void DecrementReferenceCountAndCleanup(){
+			if(refCount == nullptr){
+				return;
+			}
+
+			*refCount--;
+			if(!refCount->HasReferences()){
+				delete refCount;
+				refCount = nullptr;
+
+				delete object;
+				object = nullptr;
+			}
+		}
+	};
+}
+
+#endif //!JAMMER_MEMORY_SHARED_PTR_H
